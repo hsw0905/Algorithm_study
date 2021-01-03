140d444e85c5e63a73e693f0991d8eb0af77854f
hsw0905 <hsw0905@gmail.com>
2021-01-03 18:16:41 +0900

    coding arraylist in c

---
+++ data_structure/list/array/arraylist.c
@@ -0,0 +1,164 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+	unsigned int length;
+	unsigned int capacity;
+	int *pArray;
+} arrayList_t;
+
+arrayList_t *createList(unsigned int capacity)
+{
+	int i;
+	arrayList_t *newList = (arrayList_t *)malloc(sizeof(arrayList_t));
+	newList->capacity = capacity;
+	newList->length = 0;
+	newList->pArray = malloc(sizeof(int) * capacity);
+
+	return newList;
+}
+
+void freeList(arrayList_t *list)
+{
+	free(list->pArray);
+	free(list);
+}
+
+arrayList_t *expandList(arrayList_t *list)
+{
+	int i;
+	arrayList_t *newList = (arrayList_t *)malloc(sizeof(arrayList_t));
+	newList->capacity = list->capacity * 2;
+	newList->length = list->length;
+	newList->pArray = malloc(sizeof(int) * newList->capacity);
+	newList->pArray = NULL;
+
+	for (i = 0; i < newList->length; i++)
+	{
+		newList->pArray[i] = list->pArray[i];
+	}
+
+	freeList(list);
+	list = NULL;
+
+	return newList;
+}
+
+int isFull(arrayList_t *list)
+{
+	if (list->capacity <= list->length)
+		return 1;
+
+	return 0;
+}
+
+int append(arrayList_t *list, int data)
+{
+	if (isFull(list))
+	{
+		printf("Can't append data, list is full\n");
+		return 0;
+	}
+	else
+	{
+		list->pArray[list->length] = data;
+		list->length++;
+		return 1;
+	}
+}
+
+int prepend(arrayList_t *list, int data)
+{
+	int i;
+	if (isFull(list))
+	{
+		printf("Can't prepend data, list is full\n");
+		return 0;
+	}
+	else
+	{
+		for (i = list->length; i >= 0; i--)
+		{
+			list->pArray[i + 1] = list->pArray[i];
+		}
+		list->pArray[0] = data;
+		list->length++;
+
+		return 1;
+	}
+}
+
+int insert(arrayList_t *list, int data, int index)
+{
+	int i;
+	if (isFull(list))
+	{
+		printf("Can't prepend data, list is full\n");
+		return 0;
+	}
+	else if (list->length < index)
+	{
+		printf("Can't prepend data, out of index\n");
+		return 0;
+	}
+	else
+	{
+		for (i = list->length; i > index; i--)
+		{
+			list->pArray[i + 1] = list->pArray[i];
+		}
+		list->pArray[index] = data;
+		list->length++;
+
+		return 1;
+	}
+}
+
+int rmData(arrayList_t *list, int index)
+{
+	int i;
+	if (list->length < index)
+	{
+		printf("Can't remove data, out of index\n");
+		return 0;
+	}
+	else
+	{
+		for (i = list->length; i > index; i--)
+		{
+			list->pArray[i - 1] = list->pArray[i];
+		}
+		// list->pArray[list->length] = '\0';
+		list->length--;
+
+		return 1;
+	}
+}
+
+void displayList(arrayList_t *list)
+{
+	int i;
+	printf("List info\n");
+	printf("capacity: %d, length: %d\n", list->capacity, list->length);
+	printf("data\n");
+	for (i = 0; i < list->length; i++)
+	{
+		printf("array[%d]: %d\n", i, list->pArray[i]);
+	}
+}
+
+int main()
+{
+	arrayList_t *mylist = createList(10);
+	append(mylist, 1);
+	append(mylist, 2);
+	append(mylist, 3);
+	prepend(mylist, 4);
+	insert(mylist, 5, 2);
+	// todo : insert , remove
+	// insert(mylist, 6, 1);
+	// rmData(mylist, 1);
+	displayList(mylist);
+	freeList(mylist);
+}
